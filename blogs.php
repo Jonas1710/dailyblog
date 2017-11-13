dcd32e2788471d85e94ca8a5302d3ae01069e295
jonas1710 <jonas.mollet53@gmail.com>
2017-11-13 12:24:09 +0100

    Blogs Links

--- blogs.php
+++ blogs.php
@@ -5,14 +5,7 @@
   // Schlaufe über alle Blogs bzw. Benutzer
   foreach ($blogs as $blog) {
 	?>
-		<div><a href=index.php?function=blogs&bid='1'><h4> <?php echo getUserName($uid);  ?></h4></a></div>
+		<div><a href="index.php?function=blogs&bid=<?php echo $blog['uid']; ?>"><h4> <?php echo $blog['name']; ?> </h4></a></div>
 <?php
-//<?php $blog[$blogId]
   }
 	?>
-
-
-	<div><a href='index.php?function=blogs&bid=4' title='Blog auswählen'><h4>Anna Abegglen</h4></a></div>
-	<div><a href='index.php?function=blogs&bid=2' title='Blog auswählen'><h4>Hans Hinterseer</h4></a></div>
-	<div><a href='index.php?function=blogs&bid=1' title='Blog auswählen'><h4>Marc Muster</h4></a></div>
-	<div><a href='index.php?function=blogs&bid=3' title='Blog auswählen'><h4>Sonja Sauser</h4></a></div>
